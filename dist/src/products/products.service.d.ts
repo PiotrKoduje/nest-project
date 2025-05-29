4c26ba9da284ad7bb922fe7550c9dc80b67b941f
Piotr Pelc <piotrpelc83@gmail.com>
2025-05-30 01:35:30 +0200

    Add orders module and products endpoint create

--- dist/src/products/products.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { Product } from './../db';
-export declare class ProductsService {
-    getAll(): Product[];
-    getById(id: Product['id']): Product | null;
-    deleteById(id: Product['id']): void;
-}
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Param, Delete} from '@nestjs/common';
+import { Controller, Get, Param, Delete, Post, Body } from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { Product } from 'src/db';
+import { CreateProductDTO } from './dtos/create-product.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -18,7 +20,12 @@ export class ProductsController {
 
   @Delete('/:id')
   deleteById(@Param('id') id: string) {
-  this.productsService.deleteById(id);
-  return { success: true };
-}
+    this.productsService.deleteById(id);
+   return { success: true };
+  }
+
+  @Post('/')
+  create(@Body() productData: Omit<Product, 'id'>) {
+    return this.productsService.create(productData);
+  }
 }
