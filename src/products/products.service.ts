e767a62cc71401fb0fde87c7944c84baa0c6d740
Piotr Pelc <piotrpelc83@gmail.com>
2025-06-03 16:22:14 +0200

    Switch data from db.ts to MySql via Prisma for ProductsModule

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,32 +1,40 @@
 import { Injectable } from '@nestjs/common';
-import { db, Product } from './../db';
-import { v4 as uuidv4 } from 'uuid';
+import { Product } from '@prisma/client';
+import { PrismaService } from 'src/shared/services/prisma.service';
 
 @Injectable()
 export class ProductsService {
+  constructor(private prismaService: PrismaService) {}
   
-  getAll(): Product[] {
-    return db.products;
+  getAll(): Promise<Product[]> {
+    return this.prismaService.product.findMany();
   }
 
-  getById(id: Product['id']): Product | null {
-    return db.products.find((p) => p.id === id);
+  getById(id: Product['id']): Promise<Product | null> {
+    return this.prismaService.product.findUnique({
+      where: { id },
+    });
   }
 
-  deleteById(id: Product['id']): void {
-    db.products = db.products.filter((p) => p.id !== id);
+  deleteById(id: Product['id']): Promise<Product> {
+    return this.prismaService.product.delete({
+      where: { id }
+    })
   }
 
-  create(productData: Omit<Product, 'id'>): Product {
-    const newProduct = { ...productData, id: uuidv4() };
-    db.products.push(newProduct);
-    return newProduct;
+  create(productData: Omit<Product, 'id' | 'createdAt' | 'updatedAt'>): Promise<Product> {
+    return this.prismaService.product.create({
+      data: productData
+    });
   }
 
-  updateById(id: Product['id'], productData: Omit<Product, 'id'>): void {
-    db.products = db.products.map(p => {
-      if (p.id === id) return { ...p, ...productData };
-      return p;
+  updateById(
+    id: Product['id'], 
+    productData: Omit<Product, 'id' | 'createdAt' | 'updatedAt'>
+  ): Promise<Product> {
+    return this.prismaService.product.update({
+      where: { id },
+      data: productData
     });
   }
 }
